054ba3852e7e87a64e6c09fbd51dce7bc569fe11
boltex <fmalboeuf@oceanex.com>
2018-09-13 14:04:36 -0400

    cleaned up plugin name/path.

--- www/killapplication.js
+++ www/killapplication.js
@@ -1,7 +1,15 @@
-exports.test = function () {
-    cordova.exec(function(){
-    	console.log('test success');
+exports.test = function (p_message) {
+	console.log("gonna call exec from js.");
+    cordova.exec(function(p_success){
+    	console.log('test success. Message : ', p_success);
     }, function(p_error) {
-        console.log('test error');
-    }, "Killapplication", "test", []);
+        console.log('test error', p_error);
+    }, "Killapplication", "test", [p_message]);
+};
+exports.kill = function () {
+    cordova.exec(function(p_success){
+    	console.log('kill success.');
+    }, function(p_error) {
+        console.log('kill error', p_error);
+    }, "Killapplication", "kill", []);
 };
